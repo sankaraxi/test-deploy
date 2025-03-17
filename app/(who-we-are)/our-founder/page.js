c3df636eac99cd2dd6cc44ea8449cf904ab74ed2
Sankar K G <sankarfrompalani@gmail.com>
2025-03-17 11:45:05 +0530

    our-founder3

--- app/(who-we-are)/our-founder/page.js
+++ app/(who-we-are)/our-founder/page.js
@@ -27,14 +27,14 @@ export const metadata = {
       "Dr J Vijay Venkatraman, a globally recognized pharmacovigilance leader, is committed to advancing drug safety and regulatory compliance worldwide.",
     url: "https://www.oviyamedsafe.com/our-founder",
     type: "website",
-    // images: [
-    //   {
-    //     url: "https://www.oviyamedsafe.com/LOGO.png",
-    //     width: 1200,
-    //     height: 630,
-    //     alt: "Oviya MedSafe Logo",
-    //   },
-    // ],
+    images: [
+      {
+        url: "https://www.oviyamedsafe.com/LOGO.png",
+        width: 1200,
+        height: 630,
+        alt: "Oviya MedSafe Logo",
+      },
+    ],
   },
 };
 
